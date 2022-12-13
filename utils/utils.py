a15272eb36d636554b8c7d616d3807eadda110cc
Starcea <stardev.uwu@gmail.com>
2022-12-13 18:37:21 +0900

    fix: typing issue

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from time import mktime
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Optional
 
 from discord import Embed, SelectOption
 from pytz import timezone
@@ -14,7 +14,7 @@ def datetime_to_unix(n: datetime) -> int:
     return int(mktime(n.timetuple()))
 
 
-def help_maker(raw: Dict[str: str], color: int, is_embed: bool = True) -> List[Union[Embed, SelectOption]]:
+def help_maker(raw: Dict, color: int, is_embed: Optional[bool] = True) -> List[Union[Embed, SelectOption]]:
     embed_list = []
     for title in raw:
         embed_list.append(
