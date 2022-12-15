de504614bd92141a1fbc2872af5a5a83aa0c23c0
Starcea <stardev.uwu@gmail.com>
2022-12-15 20:25:23 +0900

    feat: dev tools

--- functions/default.py
+++ functions/default.py
@@ -50,38 +50,6 @@ class Default(commands.Cog):
         embed.add_field(name="버전", value=str(VERSION), inline=False)
         await ctx.respond(embed=embed)
 
-    @slash_command()
-    async def stop(self, ctx: ApplicationContext):
-        if await self.bot.is_owner(ctx.user):
-            await ctx.respond("stopping..")
-            await self.bot.close()
-
-    @slash_command()
-    async def load_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
-        if await self.bot.is_owner(ctx.user):
-            self.bot.load_extension(f"functions.{ext_name}")
-            await ctx.respond(f"{ext_name}.py loaded")
-
-    @slash_command()
-    async def unload_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
-        if await self.bot.is_owner(ctx.user):
-            self.bot.unload_extension(f"functions.{ext_name}")
-            await ctx.respond(f"{ext_name}.py loaded")
-
-    @slash_command()
-    async def reload_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
-        if await self.bot.is_owner(ctx.user):
-            self.bot.unload_extension(f"functions.{ext_name}")
-            self.bot.load_extension(f"functions.{ext_name}")
-            await ctx.respond(f"{ext_name}.py reloaded")
-
-    @slash_command()
-    async def send_here(self, ctx: ApplicationContext, content: Option(str)):
-        if await self.bot.is_owner(ctx.user):
-            await ctx.send(content)
-            delete_this = await ctx.respond("random respond")
-            await delete_this.delete_original_response()
-
 
 def setup(bot):
     logger.info("Loaded")
